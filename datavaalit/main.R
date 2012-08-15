2c0c56617e568aee7015dbb12626b083a4b11da2
hp <lei@ubuntu>
2012-08-15 17:15:17 +0300

    Datavaalit JSON Example

--- datavaalit/main.R
+++ datavaalit/main.R
@@ -1,53 +1,27 @@
-library(sorvi)
-library(rjson)
+library(sorvi) # 0.1.81
 
-ReadDatavaalit <- function (data.id) {
-
-  # Read election info
-  if (data.id ==  "election.data") { 
-
-    f <- "http://beta.datavaalit.fi/api/v1/election/?format=json&limit=500"
-    dat <- fromJSON(paste(readLines(f), collapse = ""))
-    
-  } else if (data.id == "municipality.data") {
-
-    f <- "http://beta.datavaalit.fi/api/v1/municipality/?format=json&limit=500"
-    dat <- fromJSON(paste(readLines(f), collapse = ""))
-
-  }
-
-  dat  
-}
-
-
+# Read election data from datavaalit.fi
 election.data <- ReadDatavaalit("election.data")
+
+# Read municipality data from datavaalit.fi
 municipality.data <- ReadDatavaalit("municipality.data")
 
-
-
 # Extract voting percentages and corresponding municipality ids
-voting.percentages <- as.numeric(sapply(election.data$objects[[2]]$voting_percentage, function (x) {x$value}))
-municipality.id <- sapply(election.data$objects[[2]]$voting_percentage, function (x) {x$municipality})
-municipality.id <- sapply(strsplit(municipality.id, "/"), function (x) {x[[5]]})
+vp <- election.data$objects[[2]]$voting_percentage
+voting.percentages <- as.numeric(sapply(vp, function (x) {x$value}))
+municipality.id <- sapply(strsplit(sapply(vp, function (x) {x$municipality}), "/"), function (x) {x[[5]]})
+# FIXME: create direct way to pick the ID, either from municipality.data or election.data 
+# (these can be matched directly based on api ids)
 
 # Load Maanmittauslaitos data (C) Maanmittauslaitos 2011
 LoadData("MML")
 sp <- MML[["1_milj_Shape_etrs_shape"]]$kunta1_p
 
-# Match MML and Datavaalit data sets
+# Match MML and Datavaalit info
 mml.id <- as.numeric(as.character(as.data.frame(sp)$Kunta))
 sp$voting.percentage <- voting.percentages[match(mml.id, municipality.id)]
 
-# Visualize
+# Visualize voting percentages across Finland
 tmp <- PlotShape(sp, "voting.percentage")
 
-
-#sapply(municipality.data$objects, function (x) {x$resource_uri})
-#sapply(municipality.data$objects, function (x) {x$id})
-#sapply(municipality.data$objects, function (x) {x$name})
-#names <- sapply(municipality.data$objects, function (x) {x$name})    
-#match(as.data.frame(sp)$Kunta.FI, names)
-#setdiff(municipality.id, sp$Kunta)
-#sapply(municipality.data$objects, function (x) {x$name})
-#setdiff(sapply(municipality.data$objects, function (x) {x$name}), sp$Kunta.FI)
 
