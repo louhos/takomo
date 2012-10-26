943d147f4b376e446e69bdd3e0200f707378acf3
antagomir <lei@MIB12001>
2012-10-26 14:43:47 +0200

    some

--- datavaalit/some.R
+++ datavaalit/some.R
@@ -1,6 +1,11 @@
 some <- read.csv("http://www.datavaalit.fi/storage/some-updates-stats-2012-10-26.csv", sep = ",")
 names(some) <- c("Puolue", "Media", "dat")
 
+
 library(ggplot2)
-p <- ggplot(some, aes(x = Puolue, y = dat, group = Media)) + geom_bar()
+p <- ggplot(some, aes(x = Puolue, y = dat, group = Media)) + geom_bar(stat = "identity") + facet_grid(. ~ Media) + coord_flip()
+
+some <- some[order(some$dat),]
+some <- subset(some, Media == "FB")
+p <- ggplot(some, aes(x = Puolue, y = dat)) + geom_point()
 
