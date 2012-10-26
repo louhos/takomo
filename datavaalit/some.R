1784aa2e474d6086de233a687e8273147e49994c
antagomir <lei@MIB12001>
2012-10-26 14:37:21 +0200

    some

--- datavaalit/some.R
+++ datavaalit/some.R
@@ -1,2 +1,6 @@
 some <- read.csv("http://www.datavaalit.fi/storage/some-updates-stats-2012-10-26.csv", sep = ",")
+names(some) <- c("Puolue", "Media", "dat")
+
+library(ggplot2)
+p <- ggplot(df, aes(x = Puolue, y = dat, group = Media)) + geom_bar()
 
