d5cef859bf952ed3c75a8d1dcd3ba5196885347d
Juuso Parkkinen <juuso.parkkinen@gmail.com>
2013-02-08 14:07:05 +0200

    HSL data progressing

--- HSL-Navigator/HSL_data.R
+++ HSL-Navigator/HSL_data.R
@@ -3,7 +3,10 @@
 data.folder <- "/Users/juusoparkkinen/Documents/workspace/data/HSL_Navigator/"
 
 # Read data
-temp <- read.csv(paste(data.folder, "hastusgps20130114-20130120.rdm", sep=""), sep=";", header=FALSE)
+dat <- read.csv(paste(data.folder, "hastusgps20130114-20130120.rdm", sep=""), sep=";", header=FALSE)
+# Read column info
+temp <- scan(paste(data.folder, "hastus_kuvaus.txt", sep=""), what="character", sep=";", n=19)
+names(dat) <- temp
 
 # Read stops data
 stops <- read.csv(paste(data.folder, "google_transit/stops.txt", sep=""))
@@ -12,5 +15,8 @@ stops <- read.csv(paste(data.folder, "google_transit/stops.txt", sep=""))
 library(ggmap)
 
 map.center <- geocode("Helsinki")
+Hel
 Hel.map <- qmap(c(lon=map.center$lon, lat=map.center$lat), source="google", zoom=10)
 Hel2.map <- Hel.map + geom_point(data=stops, aes(x=stop_lon, y=stop_lat))
+
+# file.remove("ggmapTemp.png")
