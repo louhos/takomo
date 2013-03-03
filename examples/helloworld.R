29f939f333812d7b418659175771d98cf2d90250
antagomir <antagomir@opencomp>
2013-03-03 12:15:51 +0100

    helloworld päivitetty

--- examples/helloworld.R
+++ examples/helloworld.R
@@ -1,1 +1,24 @@
-20111019-PCAxis.R
+# (C) Louhos 2011-2013 / Leo
+# simple sorvi helloworld example
+# This downloads municipality information data from statfi (Tilastokeskus)
+# and produces basic visualization
+
+# Load libraries
+# sorvi installation instructions: 
+# http://louhos.github.com/sorvi/asennus.html
+library(sorvi)
+
+# Hae kuntatason tiedot. Lähteet: Tilastokeskus ja Maanmittauslaitos.
+municipality.info <- GetMunicipalityInfo()
+
+# Tutki taulukon sisaltoa
+print(names(municipality.info))
+
+# Plot example
+xvar <- "Valtionveronalaiset tulot, euroa/tulonsaaja  2010"
+yvar <- "Taajama-aste, % 1.1.2011"
+x <- municipality.info[[xvar]]
+y <- municipality.info[[yvar]]
+lab <- municipality.info[["Kunta"]]
+plot(x, y, type = "n", xlab = xvar, ylab = yvar, main = "Tulot vs. taajama-aste")
+text(x, y, labels = lab, cex = 0.8)
