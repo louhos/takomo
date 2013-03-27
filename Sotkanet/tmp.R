9470b289eecc92eae644f78910089dbbdfbb0868
antagomir <antagomir@opencomp>
2013-03-28 00:16:54 +0100

    demo examples

--- Sotkanet/tmp.R
+++ Sotkanet/tmp.R
@@ -1,3 +1,32 @@
+sotkanet.indicators <- SotkanetIndicators(type = "table")
+as.character(sotkanet.indicators[sotkanet.indicators$indicator == indicator, "indicator.title.fi"])
+
+# Sort indicators by median time correlation
+s <- names(sort(sapply(corlist, function (x) {median(na.omit(x))})))
+#> unname(sapply(s[1:10], function (nam) {as.character(unique(dats[[nam]]$indicator.title.fi))}))
+
+# Pick some indicators for closer inspection
+selected.indicators <- c("Väestö, keskiväkiluku", 
+			 "Yksityisten lääkäripalvelujen kustannukset, 1 000 euroa")
+		         #"Korkea-asteen koulutuksen saaneet, % 15 vuotta täyttäneistä", 
+			 #"16-24 -vuotiaat, % väestöstä",
+ 			 #"Korkea-asteen koulutuksen saaneet, % 15 vuotta täyttäneistä",      
+ 			 #"Muu kuin suomi, ruotsi tai saame äidinkielenä / 1000 asukasta")
+			 #"Alkoholijuomien myynti asukasta kohti 100 %:n alkoholina, litraa")
+
+# For each indicator,
+# Correlate indicators with time in each municipality
+corlist <- list()
+for (i in names(dats)) {
+  dat <- dats[[i]]; 
+  dat <- dat[order(dat$year), ]; 
+  dat <- dat[!duplicated(dat),]; 
+  spl <- split(1:nrow(dat), dat$region.title.fi); 
+  cors <- sapply(spl, function(inds) {cor(dat$year[inds], dat$primary.value[inds])})
+  corlist[[i]] <- cors
+}
+
+
 #sotkanet.indicators <- sotkanet.indicators[grep("oppilaista", sotkanet.indicators[, 2]),]
 remove <- grep("EU", sotkanet.indicators$indicator.title.fi)
 remove <- c(remove, grep("Pohjoismaat", sotkanet.indicators$indicator.title.fi))
