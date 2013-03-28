020762f1b2414e46b64edafba27503cc9d4cea5a
antagomir <antagomir@opencomp>
2013-03-28 23:47:14 +0100

    gzipped the data files

--- MML/mml-example.R
+++ MML/mml-example.R
@@ -1,58 +1,32 @@
-# helper functions
+# MML 3D surface visualization examples contributed by
+# Janne Aukia
 
-readASC = function(filename) {
-	dat = read.table(filename,sep=" ",skip=6)
-	mdat = data.matrix(dat)
-	tmdat = t(mdat)[,nrow(mdat):1]
-	return(tmdat)
-}
+# Copyright (C) 2013 Louhos <louhos.github.com>. 
+# All rights reserved.
 
-readXYZ = function(filename) {
-	library(Matrix)
+# This program is open source software; you can redistribute it and/or modify 
+# it under the terms of the FreeBSD License (keep this notice): 
+# http://en.wikipedia.org/wiki/BSD_licenses
 
-	dat = read.table(filename)
-	xp = (dat$V1-min(dat$V1))/10
-	yp = (dat$V2-min(dat$V2))/10
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
-	mat = data.matrix(spMatrix(max(xp)+1,max(yp)+1,xp+1,yp+1,dat$V3))
-	return(mat)
-}
+# Tested with sorvi 0.2.25
 
-normalizeValues = function(mat) {
-	mat = mat-min(mat)
-	mat = mat/max(mat)
-	mat = (mat*255)
-	return(mat)	
-}
-
-plotSurface = function(mat, colorlut) {
-	library(rgl)
-	dl = dim(mat)
-	nmat = normalizeValues(mat)
-	col = colorlut[nmat+1]
-	surface3d(0:(dl[1]-1),0:(dl[2]-1),mat,color=col)
-}
-
-natural.colors = function() {
-	colors = colorRampPalette(c("cornflowerblue","darkolivegreen4","chartreuse4","chocolate4"))
-	colorlut = colors(100)[c(1,seq(0,25,length.out=7),seq(25,50,length.out=80),seq(50,75,length.out=100),seq(75,100,length.out=255))] 
-	return(colorlut)
-}
-
-# SET YOUR WORKING DIR
-
-#setwd("~/Desktop/takomo/MML")
-setwd(".")
+# Install and load libraries
+if (try(library(devtools)) == "try-error") {install.packages("devtools")}
+library(devtools)
+install_github("sorvi", "louhos", ref = "develop")
+library(sorvi) # http://louhos.github.com/sorvi
 
 # KAUPPATORI - 2M MAP DATA
-
-mat = readASC("data/L4133C.asc")
+mat <- ReadASC(gzfile("data/L4133C.asc.gz"))
 # contour(mat)
-tsel = normalizeValues(mat[0:600,2400:3000])
-plotSurface(tsel/4,natural.colors())
+tsel <- NormalizeValues(mat[0:600,2400:3000])
+PlotSurface(tsel/4,natural.colors())
 
 # KAUPPATORI - 10M MAP DATA
-
-mat = readXYZ("data/L4133C.xyz")
-tsel = normalizeValues(mat[0:120,480:600])
-plotSurface(tsel/16,natural.colors())
+mat <- ReadXYZ(gzfile("data/L4133C.xyz.gz"))
+tsel <- NormalizeValues(mat[0:120,480:600])
+PlotSurface(tsel/16,natural.colors())
