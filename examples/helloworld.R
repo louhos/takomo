29e7ec612ae47560eca9cb86a5d0f06b3443d74e
Juuso Parkkinen <juuso.parkkinen@gmail.com>
2013-05-26 22:24:30 +0300

    Fixed helloworld.R again

--- examples/helloworld.R
+++ examples/helloworld.R
@@ -15,10 +15,10 @@ suppressWarnings(municipality.info <- GetMunicipalityInfo())
 print(names(municipality.info))
 
 # Plot example
-# xvar <- "Valtionveronalaiset tulot, euroa/tulonsaaja  2011"
-# yvar <- "Taajama-aste, % 1.1.2011"
-x <- municipality.info[[13]] #[[xvar]]
-y <- municipality.info[[3]]#[[yvar]]
+xvar <- 13 #"Valtionveronalaiset tulot, euroa/tulonsaaja  2011"
+yvar <- 3 #"Taajama-aste, % 1.1.2011"
+x <- municipality.info[[xvar]]
+y <- municipality.info[[yvar]]
 lab <- municipality.info[["Kunta"]]
-plot(x, y, type = "n", xlab = xvar, ylab = yvar, main = "Tulot vs. taajama-aste")
+plot(x, y, type = "n", xlab = names(municipality.info)[xvar], ylab = names(municipality.info)[yvar], main = "Tulot vs. taajama-aste")
 text(x, y, labels = lab, cex = 0.8)
