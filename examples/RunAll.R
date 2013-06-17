605643e2e9aa767df83c89a8034c1e24ab1e488b
antagomir <antagomir@opencomp>
2013-06-18 01:50:43 +0200

    ko

--- examples/RunAll.R
+++ examples/RunAll.R
@@ -3,23 +3,18 @@ fs <- list.files(pattern = ".R$")
 
 # see also ../testruns/ and merge here at some point
 
-# Files TO FIX
-# See also testing.tmp
-fixme.files <-  c()
-#   		  "20111019-PCAxis.R", # Figure does not become visible?
-#   		  "20111117-muuttoliike.R" # Figure does not become visible?
-#   )
-  
-
 # Files to SKIP from testing
 skip.files <- c(
   "20120820-aihepiirianalyysi.R", # -> too complicated to run in a batch
   "20120205-presidentti2012analysis.R", # -> requires personal API key
-  "20120115-Presidentti2012.R" # -> requires personal API key
+  "20120115-Presidentti2012.R", # -> requires personal API key
+  "20120226-kuntajako.R", # Error in rgdal::writeOGR(uusi.kuntajako, "uudet_kunnat.shp", "uusi.kuntajako",  : Creation of output file failed -> IN UBUNTU / LL
+  "20120216-KunnatJaPresidentinvaali.R", # takes ages??
+  "20120521-Kuntadata.R"  # takes ages??
 	   )
 
 
-fs <- setdiff(fs, c("RunAll.R", fixme.files, skip.files))
+fs <- setdiff(fs, c("RunAll.R", skip.files))
 
 # Run all R files
 # for (f in sample(fs)) { 
