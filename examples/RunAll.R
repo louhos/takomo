c5300df5dd5c0e53fd4be1fb59347bf7efe2ea20
antagomir <antagomir@opencomp>
2013-08-28 22:58:21 +0300

    functions from sorvi

--- examples/RunAll.R
+++ examples/RunAll.R
@@ -13,7 +13,9 @@ skip.files <- c(
   "20120521-Kuntadata.R",  # takes ages??
   "20111005-HSOpen3_lammittely.R", #Error in download.file(url, destfile, mode = "wb", quiet = TRUE) : cannot open URL 'http://maps.google.com/maps/api/staticmap?center=60.2,24.93&zoom=11&size=640x640&maptype=Map&format=png32&sensor=true' In addition: Warning message: In download.file(url, destfile, mode = "wb", quiet = TRUE) : cannot open: HTTP status was '403 Forbidden'
   "20111010-HSOpen3_raportti.R", # Error in download.file(url, destfile, mode = "wb", quiet = TRUE) :   cannot open URL 'http://maps.google.com/maps/api/staticmap?center=60.2,24.93&zoom=11&size=640x640&maptype=Map&format=png32&sensor=true'
-  "20120629-MMLcoast.R" # Never completes..
+  "20120629-MMLcoast.R", # Never completes..
+  "20120911-MOT.R", # Never completes..
+  "20111023-oikotie.R" # Oikotie deprecated from sorvi
 	   )
 
 
---
+++ sorvi-functions/show-methods.R
@@ -0,0 +1,17 @@
+# This file is a part of the soRvi program (http://louhos.github.com/sorvi/)
+
+# Copyright (C) 2010-2012 Louhos <louhos.github.com>. All rights reserved.
+
+# This program is open source software; you can redistribute it and/or modify 
+# it under the terms of the FreeBSD License (keep this notice): 
+# http://en.wikipedia.org/wiki/BSD_licenses
+
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+
+#setMethod(f = "show", signature("NetResponseModel"),
+#  function( object ){cat("NetResponseModel object\n")}
+#)
+NULL
