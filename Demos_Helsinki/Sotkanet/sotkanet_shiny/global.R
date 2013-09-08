d5c3c1379fd00403ad1c34afe197804c9207da6b
Juuso Parkkinen <juuso.parkkinen@gmail.com>
2013-09-08 15:03:40 +0300

    Added Demos sotkanet preprocessing and shiny codes

---
+++ Demos_Helsinki/Sotkanet/sotkanet_shiny/global.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(googleVis)
+library(reshape2)
+library(ggplot2)
+library(grid)
+setwd("/Users/juusoparkkinen/Documents/workspace/Rdrafts/demos/sotkanet/sotkanet_shiny/")
+
+load("../sotkanet_data_20130908.RData")
+load("../Sotkanet_MunicipalityData_20130908.RData")
---
+++ Demos_Helsinki/Sotkanet/sotkanet_shiny/server.R
@@ -0,0 +1,76 @@
+library(shiny)
+
+
+shinyServer(function (input, output) {
+
+  data.mc <- reactive({
+
+    # Use given region cateogry
+    sotkanet.df <- sotkanet.list[[input$region.category]]
+    # Take subset based on chosen indicator categories (only when input given)
+    if (!is.null(input$indicator.category)) {
+      indicator.categories <- c("YLEISET", input$indicator.category)
+      message(indicator.categories)
+      inds.totake <- unlist(lapply(indicator.categories, function(x) grep(x, sotkanet.df$Muuttuja)))
+      sotkanet.df <- droplevels(sotkanet.df[inds.totake,])
+    }
+    return(sotkanet.df)
+  })
+  
+  data.map.gvis <- reactive({
+    
+    # Use data for given indicator and year
+    map.df <- droplevels(subset(sotkanet.list$maakunta, 
+                                Muuttuja == input$map.indicator & Vuosi == input$map.year))
+    map.df <- merge(map.df, unique(kunnat[c("maakunta.title.fi", "Code")]), 
+                    by.x="Maakunta", by.y="maakunta.title.fi")#, all.y=TRUE)
+    
+    return(map.df)
+  })
+  
+  data.map.ggplot <- reactive({
+    
+    # Use data for given indicator (all available years)
+    map.df <- droplevels(subset(sotkanet.list$maakunta, Muuttuja==input$map.indicator))
+    map.df <- merge(map.df, unique(kunnat[c("maakunta.title.fi", "Code")]), 
+                    by.x="Maakunta", by.y="maakunta.title.fi", all.y=TRUE)
+    map.df <- merge(mk.df, map.df, all.x=TRUE)
+
+    return(map.df)
+  })
+  
+  output$motionchart <- renderGvis({
+        
+    sotkanet.wide <- reshape2::dcast(data.mc(), Alue + Vuosi + Maakunta ~ Muuttuja, value.var="Arvo")
+    
+    mchart <- googleVis::gvisMotionChart(sotkanet.wide, idvar="Alue", timevar="Vuosi", 
+                                         colorvar = "Maakunta", 
+                                         sizevar = "[YLEISET] Väestö 31.12.", 
+                                         options=list(height=500, width=600))
+    return(mchart)
+  })
+  
+  output$map.gvis <- renderGvis({
+    map.df <- data.map.gvis()
+    if (nrow(map.df)==0)
+      return(NULL)
+    map <- googleVis::gvisGeoMap(map.df, locationvar="Code", numvar="Arvo", hovervar="Maakunta", options=list(region="FI"))
+    return(map)
+  })
+  
+  output$map.ggplot <- renderPlot({
+    map.df <- data.map.ggplot()
+    p.map <- ggplot2::ggplot(map.df, aes(x=long, y=lat, fill=Arvo)) + 
+      geom_polygon(aes(group=Maakunta), colour="white", size=0.1) + 
+      facet_wrap(~ Vuosi, ncol=11) + 
+      theme(legend.position="top", axis.text.x=element_blank(), axis.ticks.x=element_blank(), 
+            axis.text.y=element_blank(), axis.ticks.y=element_blank(), 
+            legend.key.width = unit(2, "cm"), legend.direction="horizontal") + 
+      labs(x=NULL, y=NULL, fill=NULL) + 
+      coord_equal() + 
+      ggtitle(levels(map.df$Muuttuja)[1])
+    print(p.map)
+  })
+    
+  outputOptions(output, "motionchart", suspendWhenHidden = FALSE)
+})
