1512e5367b509a643dfef3ba79fd6883e4ef58fb
antagomir <lei@MIB12001>
2013-11-28 13:07:54 +0100

    examplë́

--- MML/example.R
+++ MML/example.R
@@ -5,14 +5,20 @@ install_github(repo = "sorvi", username = "louhos", ref = "develop")
 # Load sorvi
 library(sorvi, quietly = TRUE)
 
-# List available 2013 MML datasets in RData format
-list_mml_datasets() 
-
-# Note there are also 2012 datasets in a separate
+# There are also 2012 datasets in a separate
 # directory that is (currently) not shown in the listing.
 # Get a specific 2012 map (see list_mml_datasets() for 2013 options)
+# Browse http://www.datavaalit.fi/storage/avoindata/mml/rdata/2012/
+# for full list of data sets
+
+# Municipality borders
 map.id <- "2012/Yleiskartta-4500"
 data.id <- "kunta4_p"
+sp <- LoadMML(map.id, data.id) 
+
+# Map without sea regions
+map.id <- "2012/Maa-alueet"
+data.id <- "kuntarajat.maa.shp"
 sp <- LoadMML(map.id, data.id) 
 
 # Investigate available variables in this map
