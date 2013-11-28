8f73e859ba42ca3d379ea7849ef6afeb3a1a2c05
antagomir <lei@MIB12001>
2013-11-28 12:52:41 +0100

    example

--- MML/example.R
+++ MML/example.R
@@ -1,11 +1,18 @@
+# Get the latest sorvi development version
 library(devtools)
 install_github(repo = "sorvi", username = "louhos", ref = "develop")
 
+# Load sorvi
 library(sorvi, quietly = TRUE)
-list_mml_datasets()
 
-# Get a specific map (see list_mml_datasets() for options)
-map.id <- "Yleiskartta1000"
+# List available 2013 MML datasets in RData format
+list_mml_datasets() 
+
+# Note there are also 2012 datasets in a separate
+# directory that is (currently) not shown in the listing.
+# Get a specific 2012 map (see list_mml_datasets() for 2013 options)
+map.id <- "2012/Yleiskartta-4500"
+data.id <- "kunta4_p"
 sp <- LoadMML(map.id, data.id) 
 
 # Investigate available variables in this map
@@ -14,7 +21,6 @@ print(varnames)
 head(as.data.frame(sp))
 
 # Visualize the shape file
-p <- PlotShape(sp, "Maakunta", plot = FALSE)
+p <- PlotShape(sp, "Kunta", plot = FALSE)
 p
-
 
